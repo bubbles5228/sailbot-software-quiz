2558fc766d542190df37c35f4876d734c1531b66
bubbles5228 <lue.chen.chn@gmail.com>
2025-09-01 10:50:53 -0700

    Update standard_calc.py

--- python/standard_calc.py
+++ python/standard_calc.py
@@ -11,8 +11,19 @@ def bound_to_180(angle):
     Returns:
         float: The bounded angle in degrees.
     """
-    return 0
 
+    if angle < 180 and angle >= -180:
+        return angle
+    elif angle >= 180: # 180 becomes -180
+        while angle >= 180: # while loop in the case of angle >= 540
+            angle = angle - 360
+        return angle
+    elif angle < -180:
+        while angle < -180: # while loop in the case of angle < -540
+            angle = angle + 360
+        return angle
+    return 0
+    
 
 def is_angle_between(first_angle, middle_angle, second_angle):
     """Determines whether an angle is between two other angles.
@@ -29,4 +40,23 @@ def is_angle_between(first_angle, middle_angle, second_angle):
     Returns:
         bool: True when `middle_angle` is not in the reflex angle of `first_angle` and `second_angle`, false otherwise.
     """
+
+    first_angle = bound_to_180(first_angle)
+    middle_angle = bound_to_180(middle_angle)
+    second_angle = bound_to_180(second_angle)
+
+    if first_angle < second_angle:
+        if middle_angle < first_angle or middle_angle > second_angle:
+            return False
+    elif first_angle > second_angle:
+        if middle_angle > first_angle or middle_angle < second_angle:
+            return False
     return True
+
+
+if __name__ == "__main__":
+    print(bound_to_180(135)) # 135.0
+    print(bound_to_180(200)) # -160.0
+    print(is_angle_between(0, 45, 90)) # True
+    print(is_angle_between(45, 90, 270)) # False
+    input("Press Enter to exit ")
